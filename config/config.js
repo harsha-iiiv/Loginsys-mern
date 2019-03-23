72da46eeb6a83c03b4fda13358110dbc63ec1c40
Harshavardhan Surisetty <harsha19990226@GMAIL.COM>
2019-03-23 22:12:51 +0530

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  db: 'mongodb://username:password@url:port/db',
+ //db: 'mongodb+srv://name:@stacke-bew2p.gcp.mongodb.net/test?retryWrites=true',
+  db_dev: 'mongodb://localhost:27017/test'
+};
